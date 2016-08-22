93428d7abf1f5ab0889c56eb1a2cece804e40344
shenhui0509 <shenhui0509@outlook.com>
2016-08-22 17:52:23 +0800

    dp1

---
+++ dp/number_triangle.cc
@@ -0,0 +1,9 @@
+/* problem description
+ * find a path through a triangle to collect as much as point as possible
+ * 2
+ * 6 4
+ * 1 2 8
+ * 4 0 9 6
+ * 6 5 5 3 6
+ * the maximum points you can get is 28
+ */
