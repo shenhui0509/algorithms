1e9ac2db8915f335ce75dac958a05876c184533a
shenhui0509 <shenhui0509@outlook.com>
2016-08-23 09:40:27 +0800

    Create levenshtein.cc

---
+++ hiho/levenshtein.cc
@@ -0,0 +1,9 @@
+/* Levenshtein distance is the edit distance between two strings.
+ * the allowd action on string is :
+ * (1) replace ; (2) insert ; (3) delete
+ * str1 : GUMBO
+ * str2 : GAMBOL
+ * Levenshtein(str1, str2) = 2 (replace 'U' by 'A', add 'L' on str1)
+ */
+ 
+ 
